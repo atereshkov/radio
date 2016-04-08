2cc000e698ef9e8958028a019baa3f50614e12cb
handioq <ter.alexanderandr@gmail.com>
2016-04-08 18:05:04 +0300

    upd

---
+++ Sort/NameComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using radio.Models;
+
+namespace radio.Sort
+{
+    public class NameComparer : IComparer<Song>
+    {
+        int IComparer<Song>.Compare(Song x, Song y)
+        {
+            return x.Name.ToLower().CompareTo(y.Name.ToLower());
+        }
+
+    }
+}
---
+++ TrackOrderList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radio
+{
+    class TrackOrderList
+    {
+        public List<TrackOrder> trackOrders { get; set; }
+
+        public TrackOrderList()
+        {
+            trackOrders = new List<TrackOrder>();
+        }
+
+        public void add(TrackOrder trackOrder)
+        {
+            trackOrders.Add(trackOrder);
+        }
+
+    }
+}
